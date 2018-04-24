80ca5a4e3abbfd8bbff0c268a05e77e006403291
Sadie <sadiefreeman@gmail.com>
2018-04-24 21:10:33 +0000

    Output sum, average, and counts

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+sum = Store.sum(:annual_revenue)
+puts "The sum of annual revenue is #{sum}"
+
+avg = Store.average(:annual_revenue)
+puts "The average annual revenue is #{avg}"
+
+@big_stores = Store.where("annual_revenue > ?", 1000000).count
+puts "Number of stores generating over $1M #{@big_stores}"
