d581b0e5fa40b80aec771fbe6205068cd9246900
Sadie <sadiefreeman@gmail.com>
2018-04-24 23:45:32 +0000

    Implemented validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter in your store name here:"
+user_store = $stdin.gets.chomp
+
+new_store = Store.create(name: user_store)
+puts "<------ ERROR ------>", new_store.errors.full_messages
+
